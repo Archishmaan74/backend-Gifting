d3aa8d1660925903f93cebcc323a24aab001b332
Archishmaan Upadhyaya <archishmaan.upa4@gmail.com>
2023-05-15 14:42:27 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const fileUpload = require("express-fileupload");
 const path = require("path");
+const cors = require("cors");
 
 const errorMiddleware = require("./middleware/error");
 
@@ -16,6 +17,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(fileUpload());
+app.use(cors());
 
 // Route Imports
 const product = require("./routes/productRoute");
@@ -28,10 +30,12 @@ app.use("/api/v1", user);
 app.use("/api/v1", order);
 app.use("/api/v1", payment);
 
-app.use(express.static(path.join(__dirname, "../frontend/build")));
+//app.use(express.static(path.join(__dirname, "../frontend/build")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  //res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
+  res.send("App is running successfully");
 });
 
 // Middleware for Errors
